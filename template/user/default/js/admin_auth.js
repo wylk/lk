0003b423937f655910cd7f96b3e115c5e1abb2a3
test <1315783988@qq.com>
2018-06-08 18:12:22 +0800

    菜单修改

--- template/user/default/js/admin_auth.js
+++ template/user/default/js/admin_auth.js
@@ -21,7 +21,7 @@ function() {
 
     //监听提交
     form.on('submit(add)', function(data) {
-        $.post('?c=admin&a=auth', data.field, function(res) {
+        $.post(authUrl, data.field, function(res) {
             console.log(res);
             if(res.error == 0){
                 swal("友情提示！", res.msg,"success",false);
@@ -37,54 +37,21 @@ function() {
 
 });
 
-
-/*  layui.use('laydate', function(){
-    var laydate = layui.laydate;
-
-    
-    //执行一个laydate实例
-    laydate.render({
-      elem: '#start' //指定元素
+/*用户-删除*/
+function member_del(obj,id){
+    layer.confirm('确认要删除吗？',function(index){
+        $.get(delAuthUrl,{id:id},function(res){
+            console.log(res);
+            if(res.error == 0){
+                $(obj).parents("tr").remove();
+                layer.msg(res.msg,{icon:1,time:1000});
+            }else{
+                layer.msg(res.msg,{icon:4,time:1000});
+            }
+        },'json');
+      
     });
-
-    //执行一个laydate实例
-    laydate.render({
-      elem: '#end' //指定元素
-    });
-  });
-
-   /*用户-停用*/
- /* function member_stop(obj,id){
-      layer.confirm('确认要停用吗？',function(index){
-
-          if($(obj).attr('title')=='启用'){
-
-            //发异步把用户状态进行更改
-            $(obj).attr('title','停用')
-            $(obj).find('i').html('&#xe62f;');
-
-            $(obj).parents("tr").find(".td-status").find('span').addClass('layui-btn-disabled').html('已停用');
-            layer.msg('已停用!',{icon: 5,time:1000});
-
-          }else{
-            $(obj).attr('title','启用')
-            $(obj).find('i').html('&#xe601;');
-
-            $(obj).parents("tr").find(".td-status").find('span').removeClass('layui-btn-disabled').html('已启用');
-            layer.msg('已启用!',{icon: 5,time:1000});
-          }
-          
-      });
-  }*/
-
-  /*用户-删除*/
- /* function member_del(obj,id){
-      layer.confirm('确认要删除吗？',function(index){
-          //发异步删除数据
-          $(obj).parents("tr").remove();
-          layer.msg('已删除!',{icon:1,time:1000});
-      });
-  }*/
+}
 
 
 
