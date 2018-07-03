748dfc43d8363afa1d9206e2d71847d83659b40c
test <1315783988@qq.com>
2018-07-03 10:42:39 +0800

    index.php 修改

--- index.php
+++ index.php
@@ -4,15 +4,6 @@
 define('LEKA_PATH', dirname(__FILE__).'/');
 define('GROUP_NAME', 'index');
 define('USE_FRAMEWORK', true);
-
-
-
-<<<<<<< HEAD
 // qwwq人与人3454
-=======
->>>>>>> dcbd4cc52cfb7fa9f97bb90fce7120ad62fd35b9
-
-
-
 require_once LEKA_PATH.'source/init.php';
 
