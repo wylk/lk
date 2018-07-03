e4f985d6789a5662fbc68c077eea348377cab27e
hyy <lo_haoyuyan_ve@163.com>
2018-07-03 10:09:17 +0800

    65456

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
 define('LEKA_PATH', dirname(__FILE__).'/');
 define('GROUP_NAME', 'index');
 define('USE_FRAMEWORK', true);
-<<<<<<< HEAD
-// qwwq人与人fgjsdkllkrter
-=======
-.........
->>>>>>> f971d6df84b1bd35540d19c1cafacbe4e1ca33ad
+
+// qwwq人与人
 require_once LEKA_PATH.'source/init.php';
 ?>
