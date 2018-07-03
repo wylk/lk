6766169ef5c42553ca469c5db053321bf99c6748
hyy <lo_haoyuyan_ve@163.com>
2018-07-03 10:01:11 +0800

    index  test

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 define('LEKA_PATH', dirname(__FILE__).'/');
 define('GROUP_NAME', 'index');
 define('USE_FRAMEWORK', true);
-qwwq人与人
+// qwwq人与人fgjsdkllk
 require_once LEKA_PATH.'source/init.php';
 ?>
