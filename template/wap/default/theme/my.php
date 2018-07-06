5cb341b0b0100223dadf7bb76ec02d8f47d345ae
test <1315783988@qq.com>
2018-07-06 13:44:29 +0800

    解决冲突

--- template/wap/default/theme/my.php
+++ template/wap/default/theme/my.php
@@ -30,13 +30,8 @@
         <div class="layui-colla-item">
             <h2 class="layui-colla-title"><a href="./cardType.php">发卡</a></h2>
         </div>
-<<<<<<< HEAD
         <div class="layui-colla-item"> 
-            <h2 class="layui-colla-title"><a href="./cardList.php">卡/券/库</a></h2>
-=======
-        <div class="layui-colla-item">
-            <h2 class="layui-colla-title"><a href="./my.php?pagetype=cardList">卡/券/库</a></h2>
->>>>>>> d782c9d3f5aabe6b6ec84edc6417f02ebabed5c0
+            <h2 class="layui-colla-title"><a href="./cardList.php">卡/券/库</a></h2> 
         </div>
         <div class="layui-colla-item">
             <h2 class="layui-colla-title">API接口</h2>
