8a0a701eb470c8c8b900a492dd8cc71d1fba94a0
hyy <lo_haoyuyan_ve@163.com>
2018-07-09 16:16:58 +0800

    卡包展示页面

--- wap/cardRecord.php
+++ wap/cardRecord.php
@@ -1,4 +1,9 @@
 <?php 
 require_once dirname(__FILE__).'/global.php';
+if(empty($wap_user)) redirect('./login.php?referer='.urlencode($_SERVER['REQUEST_URI']));
+$userId = $wap_user['userid'];
+
+$res = D("Orders")->where()->select();
+
 
 include display("cardRecord");
