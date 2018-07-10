f6c52adf1a52d2eb116598bdd1fa8539bdec50fe
xiaozhang123456 <1095909314@qq.com>
2018-07-10 14:02:07 +0800

    推

--- wap/receive.php
+++ wap/receive.php
@@ -2,10 +2,11 @@
 require_once dirname(__FILE__).'/global.php';
  // $uid = $_GET['uid'];
 if(IS_POST){
-    $_POST['carated_at'] = time();
+    $_POST['carated_time'] = time();
     $_POST['onumber'] = date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
     $_POST['buy_id'] = $_SESSION['admin']['id'];
-    // $_POST['uid'] = $uid;
+    $_POST['sole_id'] = $_POST['uid'];
+    unset($_POST['uid']);
     // dump($_POST);
     if(D('Orders')->data($_POST)->add()){
        dexit(['error'=>0,'msg'=>'购买成功']);
@@ -13,6 +14,8 @@ if(IS_POST){
        dexit(['error'=>1,'msg'=>'购买失败']);
     }
 }
+
+
 $UserAud = D("Card_transaction")->where(array('id'=>$_GET['id']))->find();
 
 include display('receive');
