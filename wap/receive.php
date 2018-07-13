b11a5bc23c0b33e91ae2fb716e23be78f741921b
xiaozhang123456 <1095909314@qq.com>
2018-07-13 17:16:03 +0800

    模拟测试2

--- wap/receive.php
+++ wap/receive.php
@@ -15,42 +15,38 @@ if(IS_POST){
     $data['tran_id'] = $datas['tranId'];
     $data['carated_time'] = time();
     $data['onumber'] = date('Ymd').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
+
     if($data['number'] <= $_POST['quantity']){
         $order_id = D('Orders')->data($data)->add();
-        if($order_id){
-            D('Card_transaction')->where(array('id'=>$datas['tranId']))->setInc('frozen',$datas['number']);
+        $orders = D('Card_transaction')->where(array('id'=>$datas['tranId']))->setInc('frozen',$datas['number']);
+        if($order_id && $orders){
+
             //调用支付接口上线再做
 
             //模拟支付回调
             import('LkApi');
             $api = new LkApi(['appid'=>'23432','mchid'=>'1273566173','key'=>'sdagjjjjjk']);
-            $payData['order_id'] = 201;
+            $payData['order_id'] = $order_id;
             $rwx = $api->weixinPay($payData);
             // dump($rwx);
 
-
-
-            dexit(['error'=>0,'msg'=>'购买成功']);
+            dexit(['error'=>0,'msg'=>'购买成功',"other"=>$rwx]);
         }else{
             dexit(['error'=>1,'msg'=>'购买失败']);
         }
     }
 
-
-    //添加新卡包
-    if(!D('Card_package')->where(['uid'=>$userId,'card_id'=>$UserAud['card_id']])->find()){
-        $contract = $_GET['card']?$_GET['card']:'offset';
-        $res = hook('addCardPackage',['card_id'=>$UserAud['card_id'],'uid'=>$userId],$contract);
-    }
-    import('LkApi');
-    $api = new LkApi(['appid'=>'23432','mchid'=>'1273566173','key'=>'sdagjjjjjk']);
-    $payData['order_id'] = $order_id;
-    $rwx = $api->weixinPay($payData);
-
     // die;
 }
-    $UserAud = D("Card_transaction")->where(array('id'=>$_GET['id']))->find();
+
+$UserAud = D("Card_transaction")->where(array('id'=>$_GET['id']))->find();
+//添加新卡包
+if(!D('Card_package')->where(['uid'=>$userId,'card_id'=>$UserAud['card_id']])->find()){
+    $contract = $_GET['card']?$_GET['card']:'offset';
+    $res = hook('addCardPackage',['card_id'=>$UserAud['card_id'],'uid'=>$userId],$contract);
+}
 include display('receive');
 echo ob_get_clean();
+
 
 
