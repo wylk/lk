853bc93a823275c777d945e3708d3c0aef687ee1
hyy <lo_haoyuyan_ve@163.com>
2018-07-19 13:02:11 +0800

    商家核销功能添加

--- template/wap/default/cardList.php
+++ template/wap/default/cardList.php
@@ -95,7 +95,7 @@
             </div>
         </div>
         <div class="lk-row-btns">
-            <a href="./shopCount.php" class="lk-row-btn">核销</a>
+            <a href="./shopAccount.php?id=<?php echo $value['id'] ?>" class="lk-row-btn">核销</a>
             <a href="myDeal.php?cardId=<?php echo $value['card_id']; ?>" class="lk-row-btn">我的交易</a>
             <a href="transactionRecord.php?cardId=<?php echo $value['card_id']; ?>" class="lk-row-btn">全部交易</a>
             <a href="cardRecord.php?cardId=<?php echo $value['card_id'] ?>" class="lk-row-btn">持卡记录</a>
--- wap/home.php
+++ wap/home.php
@@ -6,11 +6,14 @@ $userId = $wap_user['userid'];
 
 $storeUid = clear_html($_GET['shoreUid']);
 // 获取该店铺中发布所有的卡片
-$shoreInfos = D("Card_package")->where(['uid'=>$storeUid])->select();
+$shoreInfos = D("Card_package")->where(['uid'=>$storeUid,'is_publisher'=>1])->select();
 $cardIds = array_column($shoreInfos, "card_id");
-if(count($cardIds)>1) $where['card_id'] = ['in',$cardIds];
-else $where['card_id'] = $cardIds[0];
-$where['status'] = 0;
+if(count($cardIds)>1) {
+	$where = " card_id in ('".implode("','", $cardIds)."') and status = 0";
+}else {
+	$where['card_id'] = $cardIds[0];
+	$where['status'] = 0;
+}
 $tranList = D("Card_transaction")->where($where)->order('createtime desc')->select();
 foreach($tranList as $key=>$value){
 	if(!in_array($value['uid'],$uids)){
