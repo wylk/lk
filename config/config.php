58a28c95b5799b4cfdbb50bd64c2a5c879287b3e
test <1315783988@qq.com>
2018-08-06 17:57:12 +0800

    12

--- config/config.php
+++ config/config.php
@@ -4,11 +4,11 @@
  */
 
 return array(
-	'DB_HOST'=>'127.0.0.1',
-	//'DB_HOST'=>'172.16.21.103',
-	'DB_NAME'=>'bcc',
-	'DB_USER'=>'Zeh9orS',
-	'DB_PWD'=>'dOpHWFjXHzbesWr9',
+	//'DB_HOST'=>'127.0.0.1',
+	'DB_HOST'=>'172.16.21.103',
+	'DB_NAME'=>'lk',
+	'DB_USER'=>'root',
+	'DB_PWD'=>'root',
 	'DB_PORT'=>'3306',
 	'DB_PREFIX'=>'lk_',
 	'STATIC_RESOURCE'=>'1'
