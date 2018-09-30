bd3ee8a47814135615551c6dde1b88d89344ac4f
xiaozhang123456 <1095909314@qq.com>
2018-09-30 10:13:08 +0800

    地图位置按钮设置

--- template/wap/default/setup.php
+++ template/wap/default/setup.php
@@ -59,10 +59,10 @@
     </div>
 
 
-    <div class="item-row lk-container-flex" style="margin: 20px auto 0px;">
+    <div class="item-row lk-container-flex" style="margin: 20px auto 0px;" id="map">
         <div class="item-row-icon row center"><i class="iconfont" style="font-size: 20px;">&#xe715;</i></div>
         <div class="item-row-title row"  >地理位置设置</div>
-        <div class="item-row-arrow row center"><i class="iconfont"  id="map" style="font-size: 20px;">&#xe6a7;</i></div>
+        <div class="item-row-arrow row center" ><i class="iconfont"   style="font-size: 20px;">&#xe6a7;</i></div>
     </div>
     <hr class="layui-bg-gray">
     <div class="item-row lk-container-flex" style="margin: 0px auto">
