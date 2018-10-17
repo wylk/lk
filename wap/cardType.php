c905f3411ac3c1451f60f5f3349e512b6fbbabf0
hyy <lo_haoyuyan_ve@163.com>
2018-10-17 18:12:27 +0800

    后台卡券数据显示

--- wap/cardType.php
+++ wap/cardType.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__).'/global.php';
 
 if(empty($wap_user)) redirect('./login.php?referer='.urlencode($_SERVER['REQUEST_URI']));
-$cardRes = M("Contract")->find();
+$cardRes = D("Contract")->where(['status'=>1])->order('`sort` asc')->select();
 $res = D("Card_package")->where(['uid'=>$wap_user['userid'],"is_publisher"])->select();
 $cardtype = array_column($res, "type");
 $cardtype = array_map(function($value,$key){return $value.="Card";}, $cardtype);
