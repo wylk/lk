dbb5827b4005eb58ebdb41c320220bd44aea7f6c
hyy <lo_haoyuyan_ve@163.com>
2018-10-23 16:50:36 +0800

    查询订单代码优化

--- wap/orderList.php
+++ wap/orderList.php
@@ -4,12 +4,14 @@ require_once dirname(__FILE__).'/global.php';
 if(empty($wap_user)) redirect('./login.php?referer='.urlencode($_SERVER['REQUEST_URI']));
 $userId = $wap_user['userid'];
 
-// 未付账订单
-$where = " and out_trade_no != ''";
-$unpaidOrderList = D("Orders")->where("buy_id=".$userId." and status=0 ".$where)->order("create_time desc")->select();
-// 付款订单
-$paidOrderList = D("Orders")->where("buy_id = ".$userId." and status = 1".$where)->order("create_time desc")->select();
 // 全部订单
+$where = " and out_trade_no != ''";
 $orderList = D("Orders")->where("buy_id = ".$userId." and status in (0,1,3) ".$where)->order("create_time desc")->select();
-
+foreach ($orderList as $key => $value) {
+	if($value['status'] == '1'){
+		$paidOrderList[] = $value;  // 付款订单
+	}elseif($value['status'] == '0'){
+		$unpaidOrderList[] = $value;  // 未付账订单
+	}
+}
 include display("orderList");
