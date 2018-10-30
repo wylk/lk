96505ae51863d773433a4e5cc6e0038d18537a81
lujiang <1315783988@qq.com>
2018-10-30 10:25:49 +0800

    sdfd

--- template/wap/default/cardType.php
+++ template/wap/default/cardType.php
@@ -5,116 +5,141 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
 	<meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <link rel="stylesheet" href="<?php echo STATIC_URL;?>x-admin/css/font.css">
-    <link rel="stylesheet" href="<?php echo STATIC_URL;?>x-admin/css/xadmin.css?r=345">
-    <script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo STATIC_URL;?>x-admin/lib/layui/layui.js" charset="utf-8"></script>
+    
     <style type="text/css">
-      .dy1{
-        margin-top: 7px;
-      }
-      .dy2{
-        margin-top: 7px;
-      }
-      .dy2 img{
-        width: 85px;
-      }
-      .dy2 h2{
-        color:red;margin-left:127px;margin-top:-44px;
-      }
-      .dy3{
-        margin-top: 7px;
-      }
-      .dy4{
-        margin-top: 7px;
-      }
-      .layui-col-space10{
-        font-size: 17px;
-      }
+    html body p{
+        padding: 0px;
+        margin: 0px;  
+    }
+    html body {
+        border-color: #f0f0f0;
+        height: 100%;  
+    }
+    .container{
+        background-color: #fff;
+        width: 95%;
+        margin:5px auto;
+        font-size: 12px;
+        color: #999;
+        border:1px solid #f0f0f0;
+        height: 40px; 
+        line-height: 20px;
+    }
+    .cards{
+        min-height: 400px;
+        margin: 10px auto;
+        width: 95%;
+    }
+    .card{
+        height: 120px;
+        width: 100%;
+        border:1px solid #f0f0f0;
+        margin-bottom: 10px;
+        border-radius: 1px;
+    }
+    .card-b{
+        width: 95%;
+        margin:5px auto 0px;
+    }
+    .card_titles{
+        height: 80px;
+        display: flex;
+    }
+    .card_title{
+        height: 100%;
+        width: 30%;
+    }
+    .card_title img{
+        height: 100%;
+        width: 100%;
+    }
+    .card_title p:first-child{
+        color: #333;
+        font-size: 18px;
+        line-height: 45px;
+    }
+    .flex-grow{
+        flex-grow:1;
+        padding-left: 10px;
+    }
+    .card_infos{
+        height: 35px;
+        display: flex;
+        justify-content: space-around;
+    }
+    .card_info{
+        width: 30%;
+        line-height:38px;
+        font-size: 12px;
+        color: #999;
+        text-align: center;
+    }
 
-      .layui-btn{
-        width: 118px;
-        height:39px;
-        border: 1.3px solid #f6bc00;
-        margin-left: 20px;
-        color:#f6bc00;
-        border-radius: 5px;
-      }
+    .btn{
+        display:block;
+        color:#999;
+        text-decoration:none;
+    }
+    .btn span{
+        font-size: 15px;
+        color:#10a3d2;
+    }
     </style>
-     <script type="text/javascript" src="<?php echo STATIC_URL;?>js/common.js" charset="utf-8"></script>
-     <script type="text/javascript">
-        $(function(){
-            lk.is_weixin() && function(){
-                $('.lk-bar-nav').css('display','none');
-                $('.lk-content').css({"padding":"0px"});
-            }()
-        })
-    </script>
 </head>
 <body>
-  <header class="lk-bar lk-bar-nav">
-    <i onclick="javascript:history.back(-1);" class="iconfont" style="font-size: 20px;">&#xe697;</i>
-    <h1 class="lk-title">选择卡券</h1>
-</header>
 <div class="lk-content">
-  <div class="layui-container">
-    <div class="layui-row dy1">
-      <font size="3">
-        <i style="color:red">注 :</i>
-        <i style="margin-left: 24px;font-size: 14px;">选择要发布的会员卡有的提现时按发布的卡券比例锁住平台币;交易完成后可以解除锁定。</i>
-      </font>
+    <div class="container">
+        <span style="color:#333">&nbsp;&nbsp;&nbsp;注 :</span>
+        选择要发布的会员卡有的提现时按发布的卡券比例锁住平台币;交易完成后可以解除锁定
     </div>
-  </div>
-  <hr class="layui-bg-gray">
-
-<!-- 分割 -->
-  <?php foreach($cardRes as $k=>$v){ ?>
-  <div class="layui-container">
-    <div class="layui-row dy2">
-      <img src="http://lk.com/upload/images/000/000/001/201806/5b34850b65353.jpg">
-      <h2><?= $v['contract_name'] ?></h2>
+    <div class="cards">
+        <?php foreach($cardRes as $k=>$v){  ?>
+    
+        <div class="card">
+            <div class="card-b">
+                <div class="card_titles">
+                    <div class="card_title">
+                        <img src="<?php echo $v['wap_logo'];?>">
+                    </div>
+                    <div class="card_title flex-grow">
+                        <p> <?= $v['contract_name'] ?></p>
+                        <p style="font-size: 12px;color: #999;"><?= $v['contract_explain'] ?></p>
+                    </div>
+                </div>
+                <div class="card_infos">
+                    <div class="card_info">发布量: 100</div>
+                    <div class="card_info">好评: 80%</div>
+                    <div class="card_info">
+                         <?php if(!in_array($v['contract_title'], $cardtype)){?>
+                            <a href="javascript:;" class="btn" data-type="1" data-card="contract_<?php echo $v['contract_title']?>"><span>+</span>&nbsp;发卡</a>
+                        <?php }else{ ?>
+                            <a href="javascript:;" class="btn" data-type="2"><span>+</span>&nbsp;管理卡券</a>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+       <?php } ?>
     </div>
-  <br>
-    <div class="layui-row dy3">
-      <h3><?= $v['contract_name'] ?>：<?= $v['contract_explain'] ?></h3>
-    </div>
-    <br>
-    <div class="layui-row dy4">
-      <div class="layui-col-space10">
-        <i>发布量 100</i>
-        <i>|</i>
-        <i>好评 80%</i>
-        <?php if(!in_array($v['contract_title'], $cardtype)){?>
-        <i><button  class="layui-btn layui-btn-primary" id="contract_<?php echo $v['contract_title']?>">发布</button></i>
-        <?php }else{ ?>
-          <i><button  class="layui-btn layui-btn-primary" name='msg'>管理卡券</button></i>
-        <?php } ?>
-      </div>
-    </div>
-  </div>
-  <br>
-  <hr class="layui-bg-gray">
-  <?php } ?>
 </div>
 </body>
 </html>
+<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo STATIC_URL;?>js/common.js" charset="utf-8"></script>
 <script type="text/javascript">
-  layui.use(['form'],function(){
-    var layer = layui.layer;
-    $("button[name=msg]").bind("click",function(res){
-      // layer.alert("已经在发布中，不能重复发布");
-      window.location.href='./cardList.php';
+
+    $(function(){
+        $('.btn').click(function(){
+            var t = $(this);
+            var type = t.data('type');
+            if(type == 1){
+                var title = t.data('card');
+                var card = title.substring(9,title.indexOf("Card"));
+                console.log(card);
+                window.location.href = "./cardmaking.php?card="+card;
+            }else if(type == 2){
+                window.location.href='./cardList.php';
+            }
+        })
     })
-  })
-$("button[id^=contract_]").bind("click",function(res){
-    console.log(this);
-    var title = $(this).attr('id');
-    var pos = title.indexOf("Card");
-    var card = title.substring(9,pos);
-    // console.log(card);
-    window.location.href = "./cardmaking.php?card="+card;
-    // console.log(title);
-    // alert('this');
-})
 </script>
