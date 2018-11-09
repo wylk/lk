f0c58ef8bfc84db60b1874d7eac1c9c38b1271ff
hyy <lo_haoyuyan_ve@163.com>
2018-11-09 10:45:01 +0800

    滑动分页接口处理

--- template/wap/default/orderList.php
+++ template/wap/default/orderList.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="<?php echo STATIC_URL;?>x-admin/css/xadmin.css?r=<?php echo time();?>">
     <script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo STATIC_URL;?>x-admin/lib/layui/layui.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="<?php echo STATIC_URL;?>mui/css/mui.min.css" type="text/css">
+    <script type="text/javascript" src="<?php echo STATIC_URL;?>mui/js/mui.min.js" charset="utf-8"></script>
+
     <style type="text/css">
     body,.lk-content{ background-color: #f2f2f2;}
     .order-content{height:86px;background-color: white;border-radius: 5px;padding:8px 15px;margin-bottom:10px;display: block;}
@@ -25,6 +28,8 @@
     .layui-tab-title{color: #999;}
     .layui-tab-brief>.layui-tab-title .layui-this{color:#333;}
     .layui-tab-brief>.layui-tab-title .layui-this:after{border-bottom: 1px solid #29aee7;}
+
+    .content{border:1px solid red;height: 600px;}
     </style>
      <script type="text/javascript" src="<?php echo STATIC_URL;?>js/common.js" charset="utf-8"></script>
      <script type="text/javascript">
@@ -42,13 +47,15 @@
          <div class="layui-container">
             <div class="layui-tab layui-tab-brief" lay-filter="aduitTab">
                 <ul class="layui-tab-title" style="background-color: white;">
-                    <li class="layui-this ">全部订单</li>
-                    <li class="">未付款订单</li>
-                    <li class="">付款订单</li>
+                    <li class="layui-this " name="tab_all">全部订单</li>
+                    <li class="" name="tab_unpaid">未付款订单</li>
+                    <li class="" name="tab_paid">付款订单</li>
                 </ul>
                 <div class="layui-tab-content">
-                    <div class="layui-tab-item layui-show " >
-                        <?php foreach($orderList as $key=>$value){ ?>
+                    <!-- <div id="pullrefreshs" style="touch-action: none;" class="mui-content mui-scroll-wrapper"> -->
+                    <div class="layui-tab-item layui-show " id="pullrefreshs_all" style="touch-action: none;border:1px solid red;height: 600px;">
+                        <div id="content_all" ></div>
+                        <!-- <?php foreach($orderList as $key=>$value){ ?>
                         <a class="order-content" href="./orderDetail.php?id=<?php echo $value['id']; ?>">
                             <div class="order-line">
                                 <span>订单：<?php echo $value['onumber'] ?></span>
@@ -78,9 +85,11 @@
                                 </div>
                             </div>
                         </a>
-                        <?php } ?>
+                        <?php } ?> -->
                     </div>
-                    <div class="layui-tab-item ">
+                    <!-- <div class="layui-tab-item " id="content_unpaid"> -->
+                    <div class="layui-tab-item " id="pullrefreshs_unpaid" style="touch-action: none;border:1px solid red;height: 600px;">
+                        <div id="content_unpaid" ></div>
                         <?php foreach($unpaidOrderList as $key=>$value){ ?>
                         <a class="order-content" href="./orderDetail.php?id=<?php echo $value['id']; ?>">
                             <div class="order-line">
@@ -116,8 +125,9 @@
                         <?php } ?>
 
                     </div>
-                    <div class="layui-tab-item ">
-                        <?php foreach($paidOrderList as $key=>$value){ ?>
+                    <div class="layui-tab-item " id="pullrefreshs_paid" style="touch-action: none;border:1px solid red;height: 600px;">
+                        <div id="content_paid" ></div>
+                        <!-- <?php foreach($paidOrderList as $key=>$value){ ?>
                         <a class="order-content" href="./orderDetail.php?id=<?php echo $value['id']; ?>">
                             <div class="order-line">
                                 <span>订单：<?php echo $value['onumber'] ?></span>
@@ -144,7 +154,7 @@
                                 </div>
                             </div>
                         </a>
-                        <?php } ?>
+                        <?php } ?> -->
                     </div>
                 </div>
             </div>
@@ -152,10 +162,40 @@
     </div>
     <?php include display('public_menu');?>
 </body>
+<script type="text/javascript" src="<?php echo STATIC_URL;?>mui/js/mui.slidingPage.js?r=<?php echo time(); ?>" charset="utf-8"></script>
+<!-- <script src="<?php echo STATIC_URL;?>mui/js/mui.pullToRefresh.js?r=random()"></script> -->
 <script type="text/javascript">
   layui.use(['element'],function(){
     var element = layui.element;
   })
+// ***************** 分页 start *****************
+$(function(){
+    inter("./orderList.php",'.layui-tab-title li','#pullrefreshs_','#content_','name');
+});
+function strFunc(data){
+  var str = '';
+    str += '<a class="order-content" href="./orderDetail.php?id='+data['id']+'">';
+    str += '<div class="order-line"><span>订单：'+data['onumber']+'</span>';
+    if(data['status'] == 0){
+        str += '<span class="right" style="color: #333;"><i class="layui-icon" style="color: #29aee7;">&#xe654;</i>&nbsp;付款</span></div>';
+    }else{
+        str += '<span class="right" style="color: #29aee7;">';
+        if(data['status'] == 1) str += "交易成功";
+        if(data['status'] == 2) str += "订单取消";
+        if(data['status'] == 3) str += "已付款";
+        if(data['status'] == 4) str += "订单超时";
+        str += "</span></div>";
+    }
+    str += '<div class="order-line order-font"><span>'+data['create_time']+'</span></div>';
+    str += '<div class="order-line">';
+        str += '<div class="order-attr left order-font"><span>数量：</span><span class="order-color">'+data['number']+'</span></div>';
+        str += '<div class="order-attr left order-font"><span>价格：</span><span class="order-color">'+data['price']+'</span></div>';
+        str += '<div class="order-attr right order-color"><span class="right">总额：'+data['price']*data['number']+'</span></div>';
+    str += '</div>';
+    str += '</a>';
+  return str;
+}
+// ***************** 分页 end *****************
 </script>
-
 </html>
+
--- wap/orderList.php
+++ wap/orderList.php
@@ -4,14 +4,37 @@ require_once dirname(__FILE__).'/global.php';
 if(empty($wap_user)) redirect('./login.php?referer='.urlencode($_SERVER['REQUEST_URI']));
 $userId = $wap_user['userid'];
 
+$limit = 2;
+$where = " and out_trade_no != ''";
+$page = 1;
+if(IS_POST){
+	$type = $_GET['type'];
+	// switch ($type) {
+	// 	case 'all':
+	// 		$orderList = D("Orders")->where("buy_id = ".$userId." and status in (0,1,3) ".$where)->order("create_time desc")->limit($limit)->select();
+	// 		break;
+	// 	case 'paid':
+	// 		$orderList = D("Orders")->where("buy_id = ".$userId."and status = 1 ".$where)->order("create_time desc")->limit(5)->select();
+	// 		break;
+	// 	case 'unpaid':
+	// 		$orderList = D("Orders")->where("buy_id = ".$userId."and status = 3 ".$where)->order("create_time desc")->limit(5)->select();
+	// 		break;
+	// 	default:
+	// 		# code...
+	// 		break;
+	// }
+$orderList = D("Orders")->where("buy_id = ".$userId." and status in (0,1,3) ".$where)->order("create_time desc")->limit($limit)->select();
+	if($orderList)
+		dexit(['error'=>0,"msg"=>"数据","data"=>["data"=>$orderList,"limit"=>$limit,"page"=>$page]]);
+	dexit(['error'=>1,"msg"=>"数据","data"=>["data"=>$orderList,"limit"=>$limit,"page"=>$page]]);
+}
+
+
+
 // 全部订单
 $where = " and out_trade_no != ''";
-$orderList = D("Orders")->where("buy_id = ".$userId." and status in (0,1,3) ".$where)->order("create_time desc")->select();
-foreach ($orderList as $key => $value) {
-	if($value['status'] == '1'){
-		$paidOrderList[] = $value;  // 付款订单
-	}elseif($value['status'] == '0'){
-		$unpaidOrderList[] = $value;  // 未付账订单
-	}
-}
+$orderList = D("Orders")->where("buy_id = ".$userId." and status in (0,1,3) ".$where)->order("create_time desc")->limit(5)->select();
+$paidOrderList = D("Orders")->where("buy_id = ".$userId."and status = 1 ".$where)->order("create_time desc")->limit(5)->select();
+$unpaidOrderList = D("Orders")->where("buy_id = ".$userId."and status = 3 ".$where)->order("create_time desc")->limit(5)->select();
+// dump($orderList);
 include display("orderList");
