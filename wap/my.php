fa15ce7807f76fd60ba7f342a72bd211cc245aaa
hyy <lo_haoyuyan_ve@163.com>
2018-12-06 15:34:57 +0800

    订单超时修改

--- wap/my.php
+++ wap/my.php
@@ -9,8 +9,7 @@ $phone = isset($wap_user['phone']) ? $wap_user['phone'] : "";
 $userId = isset($wap_user['userid']) ? $wap_user['userid'] : 1;
 
 // 清除超时订单
-// $deadline_time = option('hairpan_set.expiry_time') ? option('hairpan_set.expiry_time') : 60*30;
-$deadline_time = 30;
+$deadline_time = option('hairpan_set.expiry_time') ? option('hairpan_set.expiry_time') : 60*30;
 // /************平台币清除超时订单 start ***********/
 $leka_where = ['create_time'=>['<=',time()-$deadline_time],"status"=>'0',"type"=>0];
 $leka_orders = D("Orders")->where($leka_where)->select();
@@ -67,44 +66,6 @@ if($card_orders){
 }
 // /************卡券清除超时订单  end  *************/
 
-
-// // 清除超时订单
-// $deadline = option('hairpan_set.expiry_time') ? option('hairpan_set.expiry_time') : 60*30;
-// $where = ['create_time'=>["<=",time()-$deadline],"status"=>"0",'type'=>['!=',2]];
-// $orderlist = D("Orders")->where($where)->select();
-// if($orderlist){
-// 	foreach($orderlist as $key=>$value){
-// 		$frozenNum[$value['tran_id']] += $value['number'];
-// 		$frozenNum[$value['tran_other']] += $value['number'];
-
-// 		$frozenList[$value['tran_id']] = ['id'=>$value['tran_id'],"operator"=>"-","step"=>$frozenNum[$value['tran_id']],"field"=>"frozen"];
-// 		if(isset($value['tran_other']) && $value['tran_other'] != $value['tran_id']){
-// 			$frozenList[$value['tran_other']] = ['id'=>$value['tran_other'],"operator"=>"-","step"=>$frozenNum[$value['tran_other']],"field"=>"frozen"];
-// 		}
-// 		// 市场直卖订单超时处理
-// 		if(isset($value['tran_other']) && $value['tran_other'] == $value['tran_id']){
-// 			// num+number   frozen-number
-// 			$marketSellNum[$value['sell_id']] += $value['number'];
-// 			$marketSellFrozen[$value['sell_id']] += $value['number'];
-// 			if($value['bail']){
-// 				// bail-bail    num+bail
-// 				$marketSellNum[$value['sell_id']] += $value['bail'];
-// 				$marketBailNum[$value['sell_id']] += $value['bail'];
-// 			}
-// 		}
-// 	}
-// 	foreach($marketSellNum as $key => $value){
-// 		$clearPackage[] = ['id'=>['field'=>"uid",'val'=>$key],"operator"=>"+","field"=>'num',"step"=>$marketSellNum[$key]];
-// 		$clearPackage[] = ['id'=>['field'=>"uid",'val'=>$key],"operator"=>"-","field"=>'frozen',"step"=>$marketSellFrozen[$key]];
-// 		if($marketBailNum[$key])
-// 			$clearPackage[] = ['id'=>['field'=>"uid",'val'=>$key],"operator"=>"-","field"=>'bail',"step"=>$marketBailNum[$key]];
-// 	}
-// 	$res = M("Card_transaction")->frozen($frozenList);
-// 	$additional[] = ['field'=>'type','operator'=>'=','val'=>option("hairpan_set.platform_type_name")];
-// 	$re = M("Card_package")->dataModification($clearPackage,$additional);
-// 	D("Orders")->where(['create_time'=>["<=",time()-$deadline],"status"=>"0"])->setField("status",4);
-
-// }
 
 // 钱包
 if(isset($_GET['pagetype']) && $_GET['pagetype'] == "purse"){
