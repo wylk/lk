1d53ad886d79adf90c978f0195819937a738b550
hyy <lo_haoyuyan_ve@163.com>
2019-02-25 16:14:57 +0800

    登录验证码

--- template/wap/default/login1.php
+++ template/wap/default/login1.php
@@ -1,7 +1,6 @@
 <?php if(!defined('LEKA_PATH')) exit('deny access!');?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
@@ -13,28 +12,32 @@
     <script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo STATIC_URL;?>x-admin/lib/layui/layui.js" charset="utf-8"></script>
     <style type="text/css">
-      .check_wrapper,.login_wrapper{margin: 40px 5px;padding: 10px;background: white; border-radius: 4px;padding-bottom: 35px;}
+    body{
+       background: -webkit-linear-gradient(#70d1f5, #48bee9);
+    }
+      .check_wrapper,.login_wrapper{padding: 10px;background: white;border-radius: 4px;width: 81%;margin-left: 31px;height: 229px;margin-top: -2px;}
       /*输入框*/
-      .login_block{height:100px;border:1px solid #d2d2d2;background-color: white;display: flex;flex-direction: column;justify-content: center;border-radius:4px;margin-bottom: 50px;}
-      .login_input{height: 48%;/*border:1px solid red;*/display: flex;flex-direction: row;justify-content: center;margin:0 10px;}
-      .login_line{margin:0px;padding: 0px;background:#d2d2d2;height: 1px;}
+      .login_block{height:212px;background-color: white;display: flex;flex-direction: column;justify-content: center;border-radius:4px;margin-bottom: 50px;}
+      .login_input{height: 48%;display: flex;}
+      .login_line{border-bottom: 2px solid #d2d2d2;}
       .login_input span{display: flex;align-items: center;width: 110px;flex-direction: column;justify-content: center;}
-      .login_input input{border:0px; font-size:14px;height: 100%;}
+      .login_input input{    border: 0px;
+    font-size: 14px;
+    height: 100%;
+    margin-top: 17px;}
       .login_input img{height: 80%;border-radius:4px;width: 100%;}
       /*点击按钮*/
-      .btn_block{margin:10px;}
-      .btn{border:1px solid #29aee7;height: 30px;background: #f2f2f2;width: 100%;font-size:15px;color: #333;border-radius:3px;height: 38px;}
-
+      .btn{border: 1px solid #f8fcfd;height: 30px;background: white;width: 81%;font-size: 15px;color: #8dd0f1;border-radius: 6px;height: 38px;margin-left: 29px;margin-top: 17px;}
       .btn_action{background: #a9e6ef;}
       /*短信获取按钮*/
       .check_phone{width: 165px;align-items: flex-start;}
-      .btn_msg{border:0px;width: 100%;font-size: 12px;background: white;color:#29aee7;}
-      .notice{margin:10px;padding-left:10px;}
+      .btn_msg{border:0px;width: 100%;font-size: 12px;background: white;color:#29aee7;margin-top: 26px;}
+      .notice{margin:10px;padding-left: 34px;}
       .notice span{margin-left:5px;}
       .notice i{color:#29aee7;}
       .img1{margin: 0 auto; display: block;/* position: relative;top: -80px;*/ width: 70%;}
       .mui-bar{background: #a9e6ef;}
-      .notice a{color:#29aee7;}
+      .notice a{color:white;}
       .mui-content-padded,.mui-title{color: #333;}
     </style>
     <script type="text/javascript" src="<?php echo STATIC_URL;?>js/common.js" charset="utf-8"></script>
@@ -42,43 +45,32 @@
 </head>
 
 <body style="background-color: #ececec;overflow: hidden;">
-  <div class="check_wrapper">
-    <!-- <div class="title" ></div> -->
-    <div style="height: 100px;display: flex; justify-content: center;background-image: url(../template/wap/default/images/logo.png);background-size: 60%;background-repeat: no-repeat;    background-position: center; margin-bottom: 17px;">
-      <!-- <img class="img1" src="../template/wap/default/images/logo.png"> -->
-    </div>
+ <div><img src="../template/wap/default/images/11.png" style="width: 89%;height: 22px;margin-left: 17px;
+    margin-top: 39%;"></div>
+  <div class="login_wrapper" >
     <div class="login_block">
-      <div class="login_input">
-        <input type="text" name="phone" placeholder="请输入手机号">
-      </div>
-      <hr class="login_line" />
-      <div class="login_input">
-        <input type="text" name="check" placeholder="请输入验证码">
-        <span>
-          <img onclick="this.src='login.php?action=check&'+Math.random();" src="./login.php?action=check"  />
-        </span>
-      </div>
+        <div class="login_input">
+          <input type="text" name="phone" placeholder="手机号">
+        </div>
+        <hr class="login_line" />
+        <div class="login_input">
+          <input type="text" name="check" placeholder="验证码">
+          <span>
+            <img onclick="this.src='login.php?action=check&'+Math.random();" src="./login.php?action=check"  />
+          </span>
+        </div>
+        <hr class="login_line" />
+        <div class="login_input">
+          <input type="text" name="msg_code" placeholder="短信验证码">
+          <span>
+            <button class="btn_msg">获取验证码</button>
+          </span>
+        </div>
+          <hr class="login_line" />
     </div>
-    <div class="btn_block">
-      <button id="btn_next" class="btn">下一步</button>
-    </div>
+
   </div>
-  <div class="login_wrapper" style="display: none;">
-    <div style="height: 100px;display: flex; justify-content: center;background-image: url(../template/wap/default/images/logo.png);background-size: 60%;background-repeat: no-repeat;    background-position: center; margin-bottom: 17px;"></div>
-    <div class="login_block">
-      <div class="login_input">
-        <span class="check_phone" style="width: 180px;align-items: flex-start;"></span>
-        <span><!-- 手机号： --></span>
-      </div>
-      <hr class="login_line" />
-      <div class="login_input">
-        <input type="text" name="msg_code" placeholder="请输入手机验证码">
-        <span>
-          <button class="btn_msg">获取验证码</button>
-        </span>
-      </div>
-    </div>
-    <div class="btn_block">
+     <div >
       <button id="btn_login" class="btn">登录</button>
     </div>
     <div class="notice">
@@ -87,17 +79,6 @@
         <a href="#modal">《服务条款》</a>
       </span>
     </div>
-  </div>
-<!-- 同意条款 -->
-<div id="modal" class="mui-modal">
-  <header class="mui-bar mui-bar-nav">
-    <a class="mui-icon mui-icon-close mui-pull-right" href="#modal"></a>
-    <h1 class="mui-title">《服务条款》</h1>
-  </header>
-  <div class="mui-content" style="height: 100%;overflow: scroll;background-color: #f2f2f2;">
-    <p class="mui-content-padded"><?php echo $config['reg_readme_content'];?></p>
-  </div>
-</div>
 </body>
 
 </html>
@@ -106,37 +87,6 @@
 var phone_status = check_code_status = msg_code_status= checkbox_status = 0;
 var phone = "";
 var check_code="";
-  $("#btn_next").bind("click",function(){
-    if(phone_status != 1 || check_code_status != 1) return;
-    phone = $("[name=phone]").val();
-    var phoneReg = /^1([0-9]{10})$/;
-    if(!phoneReg.test(phone)){
-      console.log("请输入正确的手机号");
-      mui.toast("请输入正确的手机号");
-      return;
-    }
-    // check_code = $("[name=check]").val();
-    if(phone.length != 11 || check_code.length != 4){
-      console.log("请检查手机号或者验证码是否正确填写");
-      mui.toast("请检查手机号或者验证码是否正确填写");
-      return;
-    }
-    //检测手机号、验证码
-    var data = {phone:phone,type:"check",check_code:check_code}
-    console.log(data);
-    $.post("./login.php",data,function(res){
-      console.log(res);
-      if(res.res){
-        var check_phone = phone.substr(0,3)+"****"+phone.substr(7);
-        $(".check_phone").html(check_phone);
-        $(".check_wrapper").hide();
-        $(".login_wrapper").show();
-      }else{
-        console.log(res.msg);
-        mui.toast(res.msg);
-      }
-    },"json");
-  })
   // 控制输入框数量
   $("[name=phone]").bind("keyup",function(){
     var phone = $(this).val();
@@ -145,7 +95,6 @@ var check_code="";
       // 验证手机号
       var phoneReg = /^1([0-9]{10})$/;
       if(!phoneReg.test(phone)){
-        $(this).css("border","1px solid red");
         return;
       }
       phone_status = 1;
@@ -156,7 +105,6 @@ var check_code="";
     }else{
       phone_status = 0;
       $("#btn_next").css("background-color","#f2f2f2");
-      $(this).css("border","1px solid red");
     }
   });
   $("[name=check]").bind("keyup",function(){
@@ -172,7 +120,6 @@ var check_code="";
     }else{
       check_code_status = 0;
       $("#btn_next").css("background","#f2f2f2");
-      $(this).css("border","1px solid red");
     }
   });
   $("[name=msg_code]").bind("keyup",function(){
@@ -186,11 +133,24 @@ var check_code="";
     }else{
       msg_code_status = 0;
       $("#btn_login").css("background","#f2f2f2");
-      $(this).css("border","1px solid red");
     }
   });
   // 获取验证码
   $(".btn_msg").bind("click",function(){
+    phone = $("[name=phone]").val();
+    var phoneReg = /^1([0-9]{10})$/;
+    if(!phoneReg.test(phone)){
+      console.log("请输入正确的手机号");
+      mui.toast("请输入正确的手机号");
+      return;
+    }
+    // check_code = $("[name=check]").val();
+    if(phone.length != 11 || check_code.length != 4){
+      console.log("请检查手机号或者验证码是否正确填写");
+      mui.toast("请检查手机号或者验证码是否正确填写");
+      return;
+    }
+
     var data = {phone:phone,type:"code",check_code:check_code};
     $.post("./login.php",data,function(res){
       console.log(res);
@@ -199,6 +159,8 @@ var check_code="";
         mui.toast(res['msg']);
       }
       if(res['result']['result']['success']){
+        countDown = 60;
+        setTime();
         console.log("验证码发送成功");
         mui.toast("验证码发送成功");
       }else{
@@ -220,8 +182,10 @@ var check_code="";
   });
   // 登录
   $("#btn_login").bind("click",function(){
+     console.log(msg_code_status);
     if(msg_code_status != 1 || checkbox_status != 1) return;
     var msg_code = $("[name=msg_code]").val();
+    phone = $("[name=phone]").val();
     var data = {phone:phone,logintype:"checkAccount",msg_code:msg_code};
     $.post("./login.php",data,function(res){
       console.log(res);
@@ -233,4 +197,18 @@ var check_code="";
       }
     },"json");
   });
+function setTime() {
+        // alert("dff");
+        if (countDown == 0) {
+            $(".btn_msg").html("获取验证码");
+            $(".btn_msg").attr("disabled", false);
+        } else {
+            $(".btn_msg").html("重新发送" + countDown);
+            $(".btn_msg").attr("disabled", true);
+            countDown--;
+            setTimeout(function() {
+                setTime();
+            }, 1000);
+        }
+    }
 </script>
